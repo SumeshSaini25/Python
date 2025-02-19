5061810dd9c46f88cf5e1a0d986c32c8fe49cf19
SumeshSaini25 <sumeshsaini0409@gmail.com>
2025-02-19 18:01:27 +0530

    Add files via upload

---
+++ bandnamegen.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Gen")
+city = input("Which City you grow up in : ")
+pet = input("Whats the name of your pet : ")
+band_name = print(f"Yoyr Band Name could be {city} {pet} ")
